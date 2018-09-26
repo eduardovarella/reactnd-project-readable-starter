d59406189e98a94332e0ffd79139284d08cf577c
Eduado Varella <eduardo.varella@bizcap.com.br>
2018-09-26 19:34:24 -0300

    Configuring Redux DevTools Extensions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
 import thunk from 'redux-thunk';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 const store = createStore(
-    reducer, applyMiddleware(thunk)
+    reducer, composeEnhancers(
+      applyMiddleware(thunk)
+    )
   )
 
 ReactDOM.render(
